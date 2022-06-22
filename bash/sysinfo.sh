f4dfee081dc96e60d693cd6ab41a6f6148c99c0f
HimanshuPatwa <200476808@student.georgianc.on.ca>
2022-06-22 04:32:34 +0000

    Lab 2 challenge script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
+
 # Getting the hostname
-echo "Hostname:     `hostname`"
+my_hostname=`hostname`
+
 # Getting the domain name using hostname utility
-echo "Domain Name: `hostname -d`"
+domainname=`hostname -d`
 
 # Getting OS name and version
-echo "Operating System name and version: `lsb_release -ds`"
+os=`lsb_release -ds`
 
-# Showing IP Addresses assigned to the system
-echo "IP Addresses: `hostname -i`"
+# Showing IP Addresses assigned to enp0s3 interface
+ip=`hostname -I`
 # Printing the disk usage
-echo -e "Root Filesystem Status:\n`df -h -t ext4`"
+file=`df -h  --total --output=avail -t ext4 |  tail -1`
+
+
+cat <<EOF
+Report for $my_hostname
+=============================
+Domain Name      : $domainname
+Operating System name and version: $os
+IP Address: $ip 
+Root Filesystem Free Space: $file
+============================
+EOF
